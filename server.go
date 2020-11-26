b227807b3f81f8e83f4109b71fab82bca4a2c14e
Robby Radhika <robby.radhika@spesolution.com>
2020-11-26 17:34:58 +0700

    install gin

--- server.go
+++ server.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+//M is exported
+type M map[string]interface{}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		data := M{"Message": "Hello", "Counter": 2}
+		return c.JSON(http.StatusOK, data)
 	})
 
 	e.GET("/test", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, test!")
+		name := c.QueryParam("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return c.String(http.StatusOK, data)
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":3001"))
 }
